6e3b53866723264b7d10844887bede28be6afcd3
Pilip.Shchasny <c-pshchasny@neogov.net>
2022-01-07 15:58:13 +0300

    pshchasny-added-a-ReactQuery-hook-that-return-data-of-products-from-fakeAPI

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
-import {useState} from 'react'
-import {useQuery} from 'react-query'
 /*Components*/
-import Drawer from '@mui/material/Drawer'
-import LinearProgress from '@mui/material/LinearProgress'
-import Grid from '@mui/material/Grid'
-import AddShoppingCart from '@mui/icons-material/AddShoppingCart'
-import Badge from '@mui/icons-material/Badge'
 /*Styles*/
-import {Wrapper} from './App.styles'
+/*Types*/
+import {ICartItem} from './types'
+import {useQuery} from "react-query";
 
-const getProducts = async () =>{
-    await (await fetch('https://fakestoreapi.com/products'))
+
+const getProducts = async (): Promise<ICartItem[]> => {
+    const response = await fetch('https://fakestoreapi.com/products')
+    return await response.json()
 }
 
 const App = () => {
+
+    const {data, isLoading, error} = useQuery<ICartItem[]>(
+        'products',
+        getProducts)
+
     return (
         <div className="App">
             work
